09eb630fbc2753c8bdd20c5c2b435ec273857483
Harvinder Singh <harvinder@binmile.com>
2026-07-02 16:19:26 +0530

    Served HTML files in response and used path nodejs module

--- Chapter 10.2 - Airbnb/utils/pathUtil.js
+++ Chapter 10.2 - Airbnb/utils/pathUtil.js
@@ -3,11 +3,11 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url) // getting file path...........................
 
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename); // getting directory path of selected file.....................
 
-const rootDir = path.join(__dirname, "..");
+const rootDir = path.join(__dirname, '../') // Root path of selected Directory...............................
 
 // module.exports = path.dirname(require.main.filename);
 export default rootDir;
