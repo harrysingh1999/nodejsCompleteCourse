f150e3bb98d6c5e94ba84dc5838ceb1d7f4a1c6a
Harvinder Singh <harvinder@binmile.com>
2026-07-14 11:48:02 +0530

    My Chapter 16 MongoDB

---
+++ My Chapter 16 - Introduction to MongoDB/routes/storeRouter.js
@@ -0,0 +1,22 @@
+// Core Modules
+import path from "path";
+
+// External Module
+import express from "express";
+export const storeRouter = express.Router();
+
+// Local Module
+import { getIndex, getHomes,
+   getFavouriteList,
+   getBookings, getHomeDetails, 
+   postAddToFavourite, 
+   postRemoveFromFavourite} from "../controllers/storeController.js";
+
+storeRouter.get("/", getIndex);
+storeRouter.get("/homes", getHomes);
+storeRouter.get("/homes/:homeId", getHomeDetails);
+storeRouter.get("/favourites", getFavouriteList);
+storeRouter.post("/favourites", postAddToFavourite);
+storeRouter.post("/favourites/delete/:homeId", postRemoveFromFavourite);
+
+storeRouter.get("/bookings", getBookings);
